cf24283df0ed4c17e4fd9f4530dd3f1bf7edfd41
橋本 光司 <hashimoto-kouji@chuo-computer.co.jp>
2020-06-09 10:19:43 +0900

    ScrapySharpを試用

---
+++ ScrapySharpTest/ScrapySharpTest/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace ScrapySharpTest {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.TestButton = new System.Windows.Forms.Button();
+            this.ResultText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TestButton
+            // 
+            this.TestButton.Location = new System.Drawing.Point(73, 40);
+            this.TestButton.Name = "TestButton";
+            this.TestButton.Size = new System.Drawing.Size(110, 30);
+            this.TestButton.TabIndex = 0;
+            this.TestButton.Text = "Test";
+            this.TestButton.UseVisualStyleBackColor = true;
+            this.TestButton.Click += new System.EventHandler(this.TestButton_Click);
+            // 
+            // ResultText
+            // 
+            this.ResultText.Location = new System.Drawing.Point(73, 76);
+            this.ResultText.Multiline = true;
+            this.ResultText.Name = "ResultText";
+            this.ResultText.Size = new System.Drawing.Size(628, 362);
+            this.ResultText.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultText);
+            this.Controls.Add(this.TestButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TestButton;
+        private System.Windows.Forms.TextBox ResultText;
+    }
+}
+
---
+++ ScrapySharpTest/ScrapySharpTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using ScrapySharp.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScrapySharpTest {
+    public partial class Form1 : Form {
+
+        private string _rootUrl = "http://078134995:Ais5vs2004@192.168.13.67/docs/html/kitei/index.htm";
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void TestButton_Click(object sender, EventArgs e) {
+            var browser = new ScrapingBrowser();
+            browser.AllowAutoRedirect = true;
+            browser.AllowMetaRedirect = true;
+
+            var pageReult = browser.NavigateToPage(new Uri(_rootUrl));
+
+            this.ResultText.Text = pageReult.Content;
+        }
+    }
+}
